548dec1fa6d4f285f3ce8969861500e8b70a0e86
akshitasharmamca2025-jpg <akshita.sharma.mca.2025@miet.ac.in>
2026-02-22 20:21:50 +0530

    Add files via upload

---
+++ pythondataastucture.py/listandlistindexing.py
@@ -0,0 +1,24 @@
+# Program to demonstrate List and List Indexing
+
+# create a list
+numbers = [10, 20, 30, 40, 50]
+
+# printing complete list
+print("Complete List:", numbers)
+
+# accessing elements using positive indexing
+print("First element:", numbers[0])
+print("Second element:", numbers[1])
+print("Third element:", numbers[2])
+
+# accessing elements using negative indexing
+print("Last element:", numbers[-1])
+print("Second last element:", numbers[-2])
+
+# Output:
+# Complete List: [10, 20, 30, 40, 50]
+# First element: 10
+# Second element: 20
+# Third element: 30
+# Last element: 50
+# Second last element: 40
