27ea7fb472bebcd4658fa1a801da2701e2b3e4bb
akshitasharmamca2025-jpg <akshita.sharma.mca.2025@miet.ac.in>
2026-02-22 20:17:18 +0530

    Add files via upload

---
+++ classes.py/agevalidation.py
@@ -0,0 +1,6 @@
+age = int(input("Enter your age: "))
+
+if age >= 18:
+    print("Valid age. You are eligible.")
+else:
+    print("Invalid age. You are not eligible.")
---
+++ classes.py/classstudentdistribution.py
@@ -0,0 +1,16 @@
+marks = int(input("Enter marks: "))
+
+if marks >= 75:
+    print("Distinction")
+
+elif marks >= 60:
+    print("First Class")
+
+elif marks >= 50:
+    print("Second Class")
+
+elif marks >= 40:
+    print("Pass")
+
+else:
+    print("Fail")
